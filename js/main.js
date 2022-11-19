eda6d08872075230c5aa8b4fabc36ae8f7c56199
root <root@ip-172-31-42-250.us-east-2.compute.internal>
2022-11-19 00:56:19 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+/*********************************************************************************
+
+	Template Name: Template Full Name Here
+	Description: A perfect template to build beautiful and unique medical websites. It comes with nice and clean design.
+	Version: 1.0
+
+	Note: This is main js.
+
+**********************************************************************************/
+
+/**************************************************************
+	
+	MAIN JS INDEXING
+	
+	
+	
+	
+	__ END MAIN JS INDEXING
+
+***************************************************************/
+
+
+(function($){
+    'use strict';
+    
+    new WOW().init();
+
+    /**
+    * easeScroll
+    */
+    $("body").easeScroll({
+    	frameRate: 60,
+    	animationTime: 1500,
+    	stepSize: 100,
+    	pulseAlgorithm: !0,
+    	pulseScale: 8,
+    	pulseNormalize: 1,
+    	accelerationDelta: 20,
+    	accelerationMax: 1,
+    	keyboardSupport: !0,
+    	arrowScroll: 50
+    });
+
+
+})(jQuery);
